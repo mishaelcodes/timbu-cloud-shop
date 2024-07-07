7a60abf1a8cc356e5089eb4bb6b8e32372f90101
Mishael Enyi <mishaelcodes@outlook.com>
2024-07-07 11:20:19 +0100

    hero section complete

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,3 +1,5 @@
+import ellipses from "../assets/icon/ellipses.png"
+import cookingPot from "../assets/images/cooking-pot.png"
 import hamMenu from "../assets/icon/ham-menu.png";
 import heroImg from "../assets/images/hero-img.png";
 import Button from "./button";
@@ -21,6 +23,18 @@ const Hero = () => {
             everything you need to elevate your cooking experience.
           </p>
           <Button content="Shop Now" />
+        </div>
+        <div className="flex items-center justify-center flex-col bg-timbuBlue w-[95%] rounded-3xl py-2 px-7 my-12">
+          <div className="flex items-center justify-between">
+            <div className="flex items-start justify-evenly flex-col">
+              <p className="text-[8px]">Amazing limited time offer deals</p>
+              <p className="text-xs font-semibold my-1">LE CREUSET COOKWARE</p>
+              <p className="text-[8px] mb-1">Up to 60% off!</p>
+              <p className="text-[6px]">Up to 60% off!</p>
+            </div>
+            <img src={cookingPot} alt="cooking-pot" />
+          </div>
+          <img src={ellipses} alt="" />
         </div>
       </section>
     </>
