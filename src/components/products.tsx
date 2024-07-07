4edb4ff1745f15a714d09f31efe131bf49f3cc09
Mishael Enyi <mishaelcodes@outlook.com>
2024-07-07 12:26:30 +0100

    added some products

--- src/components/products.tsx
+++ src/components/products.tsx
@@ -1,4 +1,69 @@
-const Products = () => { 
-  
-} 
- export default Products
+import abMug from "../assets/images/products/AB-mug.png";
+import blueMonday from "../assets/images/products/blue-monday-ab-mug.png";
+import classicAbMugPink from "../assets/images/products/classic-ab-mug-pink.png";
+import classicAbMugBrown from "../assets/images/products/classic-ab-mug-brown.png";
+import classicAbMugYellow from "../assets/images/products/classic-ab-mug-yellow.png";
+import heartCup from "../assets/images/products/heart-cup.png";
+const Products = () => {
+  const arrayOfProducts = [
+    {
+      productName: "Classic AB Mug",
+      productImage: abMug,
+      productPrice: "7,499.99",
+      productRating: "4.2",
+    },
+    {
+      productName: "Classic AB Mug",
+      productImage: heartCup,
+      productPrice: "7,499.99",
+      productRating: "4.2",
+    },
+    {
+      productName: "Classic AB Mug",
+      productImage: classicAbMugBrown,
+      productPrice: "7,499.99",
+      productRating: "4.2",
+    },
+    {
+      productName: "Classic AB Mug",
+      productImage: classicAbMugPink,
+      productPrice: "7,499.99",
+      productRating: "4.2",
+    },
+    {
+      productName: "Classic AB Mug",
+      productImage: classicAbMugYellow,
+      productPrice: "7,499.99",
+      productRating: "4.2",
+    },
+    {
+      productName: "Classic AB Mug",
+      productImage: blueMonday,
+      productPrice: "7,499.99",
+      productRating: "4.2",
+    },
+  ];
+  return (
+    <div className="w-80 mx-auto">
+      <h2 className="font-semibold text-2xl mb-2">Products Category</h2>
+      <select
+        name="products"
+        id="products"
+        className="border-4 border-timbuGrey w-1/2 px-3 py-2 rounded-md"
+      >
+        <option value="mug">Mugs</option>
+      </select>
+      <div className="grid grid-cols-2 mt-10 gap-3">
+        {arrayOfProducts.map((product, index) => (
+          <div key={index} className="border-2 border-timbuGrey rounded-md">
+            <img src={product.productImage} alt="" />
+            <p>{product.productName}</p>
+            <p>{product.productPrice}</p>
+            <p>{product.productRating}</p>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+export default Products;
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,11 @@
 import Hero from "../components/hero";
+import Products from "../components/products";
 
 const Home = () => {
   return (
     <>
       <Hero />
+      <Products />
     </>
   );
 };
