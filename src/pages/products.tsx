4501812aeb05c91411a85156853408f3b0937c85
Mishael Enyi <mishaelcodes@outlook.com>
2024-07-12 22:22:10 +0100

    integrated the api and displayed some data

--- src/pages/products.tsx
+++ src/pages/products.tsx
@@ -1,4 +1,4 @@
-// mobile images
+/* // mobile images
 import abMug from "../assets/images/products/abMug.png";
 import blueMonday from "../assets/images/products/blueMondayMug.png";
 import classicAbMugPink from "../assets/images/products/peachMug.png";
@@ -19,164 +19,94 @@ import theBossMug from "../assets/images/products/theBossMug.png";
 import blueCoffeeMug from "../assets/images/products/blueCoffeeMug.png";
 import whiteWideLipMug from "../assets/images/products/whiteWideLipMug.png";
 import whiteWideMug from "../assets/images/products/whiteWideMug.png";
-
+ */
 //icons
 
-import star from "../assets/icon/star.png";
+// import star from "../assets/icon/star.png";
 import Button from "../components/button";
 import { Link } from "react-router-dom";
-const Products = () => {
-  const arrayOfProducts = [
+import { useState, useEffect } from "react";
+
+interface Product {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  photos: [
     {
-      productName: "Classic AB Mug",
-      productImage: abMug,
-      productPrice: "7,499.99",
-      productRating: "4.2",
-    },
-    {
-      productName: "Red heart mug",
-      productImage: heartCup,
-      productPrice: "6,500.00",
-      productRating: "4.1",
-    },
-    {
-      productName: "Cool brown mug",
-      productImage: classicAbMugBrown,
-      productPrice: "5,500.00",
-      productRating: "4.5",
-    },
-    {
-      productName: "Peach mug",
-      productImage: classicAbMugPink,
-      productPrice: "7,500.00",
-      productRating: "4.9",
-    },
-    {
-      productName: "Happy yellow mug",
-      productImage: classicAbMugYellow,
-      productPrice: "7,000.00",
-      productRating: "4.5",
-    },
-    {
-      productName: "Yellow & orange mug",
-      productImage: yellowAndOrangeMug,
-      productPrice: "8,000.00",
-      productRating: "4.8",
-    },
-    {
-      productName: "Blue monday mug",
-      productImage: blueMonday,
-      productPrice: "9,000.00",
-      productRating: "4.0",
-    },
-    {
-      productName: "Smiley face mug",
-      productImage: smileyFaceMug,
-      productPrice: "8,000.00",
-      productRating: "4.4",
-    },
-    {
-      productName: "Plain white mug",
-      productImage: plainWhiteMug,
-      productPrice: "4,000.00",
-      productRating: "4.5",
-    },
-    {
-      productName: '"Best boss" mug',
-      productImage: bestBossMug,
-      productPrice: "6,000.00",
-      productRating: "4.1",
-    },
-    {
-      productName: "White metal mug",
-      productImage: whiteMetalMug,
-      productPrice: "5,500.00",
-      productRating: "4.1",
-    },
-    {
-      productName: "Winky face mug",
-      productImage: winkyFaceMug,
-      productPrice: "6,500.00",
-      productRating: "3.1",
-    },
-    {
-      productName: "Breakfast mug",
-      productImage: breakfastMug,
-      productPrice: "4,500.00",
-      productRating: "3.8",
-    },
-    {
-      productName: "Black wide mug",
-      productImage: blackWideMug,
-      productPrice: "4,000.00",
-      productRating: "3.9",
-    },
-    {
-      productName: "World's best boss",
-      productImage: worldsBestBossMug,
-      productPrice: "5,000.00",
-      productRating: "4.0",
-    },
-    {
-      productName: "Slim yellow mug",
-      productImage: slimYellowMug,
-      productPrice: "6,000.00",
-      productRating: "3.6",
-    },
-    {
-      productName: "The Boss mug",
-      productImage: theBossMug,
-      productPrice: "3,000.00",
-      productRating: "3.6",
-    },
-    {
-      productName: "Blue coffee mug",
-      productImage: blueCoffeeMug,
-      productPrice: "6,800.00",
-      productRating: "3.7",
-    },
-    {
-      productName: "White wide lip mug",
-      productImage: whiteWideLipMug,
-      productPrice: "6,200.00",
-      productRating: "3.8",
-    },
-    {
-      productName: "White wide mug",
-      productImage: whiteWideMug,
-      productPrice: "6,400.00",
-      productRating: "4.8",
-    },
+      url: string
+    }
   ];
+  name: string;
+  current_price: [
+    {
+      NGN: {
+        0: number;
+        1: null;
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        2: any[];
+      };
+    }
+  ];
+}
+
+const Products = () => {
+  const [products, setProducts] = useState<Product[]>([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  const API_KEY = "90aec84e95284d7f8ce7ac982ad916a920240712130613816586";
+  const APP_ID = "RT6WUUXGARFTKO0";
+  const ORGANIZATION_ID = "7dbe45f8639b411bb715356cc3c02b37";
+  const url = `api/products?organization_id=${ORGANIZATION_ID}&Appid=${APP_ID}&Apikey=${API_KEY}`;
+  useEffect(() => {
+    const fetchData = async () => {
+      setIsLoading(true);
+      try {
+        const response = await fetch(url);
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        const data = await response.json();
+        console.log(data);
+        setProducts(data.items);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
   return (
     <div className="w-[85%] mx-auto mb-5">
       <h2 className="font-semibold text-base mt-3 mb-6 md:mt-12 md:mb-8">
         Products list <span className="text-gray-400">(20)</span>
       </h2>
-      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-3">
-        {arrayOfProducts.map((product, index) => (
-          <div
-            key={index}
-            className="border-2 border-timbuGrey rounded-md py-[10px] px-1"
-          >
-            <img src={product.productImage} alt="" />
-            <div className="mb-1">
-              <p className="font-semibold text-sm mt-1">
-                {product.productName}
-              </p>
-              <p className="text-timbuRed font-medium text-sm my-1">
-                N{product.productPrice}
-              </p>
-              <p className="font-semibold flex items-center justify-start">
+      {isLoading ? (
+        <p>Loading</p>
+      ) : (
+        <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5 gap-3">
+          {products.map((product: Product, index) => (
+            <div
+              key={index}
+              className="border-2 border-timbuGrey rounded-md py-[10px] px-1"
+            >
+              <img src={`https://api.timbu.cloud/images/${product.photos[0]?.url}`} alt="" />
+              <div className="mb-1">
+                <p className="font-semibold text-sm mt-1">{product.name}</p>
+                <p className="text-timbuRed font-medium text-sm my-1">
+                  N{product.current_price[0].NGN[0]}
+                </p>
+                {/* <p className="font-semibold flex items-center justify-start">
                 <img src={star} alt="star icon" /> {product.productRating}
-              </p>
+              </p> */}
+              </div>
+              <Link to="/cart">
+                <Button content="Add to cart" width="w-full" />
+              </Link>
             </div>
-            <Link to="/cart">
-              <Button content="Add to cart" width="w-full" />
-            </Link>
-          </div>
-        ))}
-      </div>
+          ))}
+        </div>
+      )}
     </div>
   );
 };
