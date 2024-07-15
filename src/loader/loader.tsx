dbe3516cfb887b68009e48daeff5409a5623d7da
Mishael Enyi <mishaelcodes@outlook.com>
2024-07-15 19:30:31 +0100

    edited css loader

--- src/loader/loader.tsx
+++ src/loader/loader.tsx
@@ -1,9 +1,9 @@
 import "./loader.css"
 const Loader = () => { 
   return (
-    <>
+    <div className="w-full h-[50vh] flex items-center justify-center">
       <span className="loader"></span>;
-    </>
+    </div>
   );
 } 
  export default Loader
